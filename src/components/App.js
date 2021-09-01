7508e8c4c41257bc4f74e5a8ea144a94297d5358
janis-jenny <pcardenasvm@gmail.com>
2021-09-01 01:05:28 -0500

    Modify package.json config, create app, book components and add containers files

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import BooksList from '../containers/BookList';
+import BooksForm from '../containers/BookForm';
+
+const App = () => (
+  <div>
+    <BooksForm />
+    <BooksList />
+  </div>
+);
+
+export default App;
