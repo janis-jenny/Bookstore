b7c089d97a47375016ba54ec0fb9e0ee5c22426f
janis-jenny <pcardenasvm@gmail.com>
2021-09-06 16:02:17 -0500

    Create the initial state object w default books and unique id

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import reducer from './reducers/books';
+
+const arrayIds = [];
+const createUniqueId = () => {
+  const someId = Math.floor(Math.random() * 1000);
+  if (!arrayIds.includes(someId)) {
+    arrayIds.push(someId);
+    return someId;
+  }
+  return createUniqueId();
+};
+
+const firstState = {
+  books: [{
+    id: createUniqueId(),
+    title: 'Game of Thrones',
+    category: 'Action',
+  },
+  {
+    id: createUniqueId(),
+    title: 'Dracula',
+    category: 'Horror',
+  },
+  {
+    id: createUniqueId(),
+    title: 'The Old Guard',
+    category: 'Sci-Fi',
+  },
+  ],
+};
+
+const store = createStore(reducer, firstState);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
